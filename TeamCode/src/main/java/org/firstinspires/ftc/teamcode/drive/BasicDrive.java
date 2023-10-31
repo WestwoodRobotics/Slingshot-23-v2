388847da5a5797e26678413c0032e9adfbd13d06
jamjammers <68483685+jamjammers@users.noreply.github.com>
2023-10-31 10:08:26 -0500

    field centric

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/BasicDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/BasicDrive.java
@@ -36,21 +36,41 @@ public class BasicDrive extends OpMode {
 
         //imu initialized
         imu = hardwareMap.get(IMU.class, "imu");
-
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+        DriveConstants.LOGO_FACING_DIR, DriveConstants.USB_FACING_DIR));
+        imu.initialize(parameters);
 
 
     }
     public void loop(){
         //get the input of the gamepad1 in order to move the robot
-        double inputY = -gamepad1.left_stick_y; // Y stick is reversed
-        double inputX = gamepad1.left_stick_x;
-        double inputRot = gamepad1.right_stick_x;
+        double y = -gamepad1.left_stick_y; // Y stick is reversed
+        double x = gamepad1.left_stick_x;
+        double rot = gamepad1.right_stick_x;
 
-        telemetry.addData("x y rot: ", inputX+", "+inputY+", "+inputRot);
-        leftFront.setVelocity(inputY + inputX + inputRot);
-        rightFront.setVelocity(inputY + inputX + inputRot);
-        leftBack.setVelocity(inputY + inputX + inputRot);
-        rightBack.setVelocity(inputY + inputX + inputRot);
+
+        //resets the imu if you press options
+        if(gamepad1.options) imu.resetYaw();
+
+        //sets the outputs to be field centric
+        heading = normalizedHeading();
+        x = x * Math.cos(-heading) - y * Math.sin(-heading);
+        y = y * Math.cos(-heading) + x * Math.sin(-heading);
+
+        //outputs the x, y, and rotation and the heading of the robot (after field centric)
+        telemetry.addData("x y rot: ", x+", "+r+", "+rot);
+        telemetry.addData("heading: ", heading);
+        //sets the power of the motors
+        leftFront.setVelocity(y + x + rot);
+        rightFront.setVelocity(y + x + rot);
+        leftBack.setVelocity(y + x + rot);
+        rightBack.setVelocity(y + x + rot);
     }
 
+    private double normalizedHeading(){
+        double fixedAngle = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+        while (fixedAngle > 2 * Math.PI) fixedAngle -= 2*Math.PI;
+        while (fixedAngle < 0) fixedAngle += 2*Math.PI;
+        return fixedAngle;
+    }
 }
